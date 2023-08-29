4245e3a7b3c657f50435f4d71c840d89dd145d92
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-08-30 00:06:10 +0530

    update ui and config

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,4 @@
 "use client"
-
-import ThemeSwitcher from "@/components/theme/ThemeSwitcher"
-
 export default function Home() {
 
   return (
@@ -11,7 +8,6 @@ export default function Home() {
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">Hello there</h1>
             <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <ThemeSwitcher />
           </div>
         </div>
       </div>
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: false,
+    themes: ["dracula"],
   },
 };
 export default config;
