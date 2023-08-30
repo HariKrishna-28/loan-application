85a1260497a544c158cb81b9b2ba08133585d706
Harikrishna <69629411+HariKrishna-28@users.noreply.github.com>
2023-08-30 15:24:04 +0530

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body data-theme="dark" >
+      <body data-theme="light" >
         <Navbar />
         {children}
       </body>
