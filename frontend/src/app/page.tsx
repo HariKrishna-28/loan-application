27d4235146437ea7f1eda314a07f46cc2c70dcd1
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-08-30 14:57:34 +0530

    refactor forms

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,20 @@
 "use client"
 
 import { accountingProviders } from "@/components/Constants/AccountingProviderOptions"
+import { useState } from "react"
 
 export default function Home() {
+  const [name, setName] = useState<string>("")
+  const [year, setYear] = useState<string>("")
+  const [profitSummary, setProfitSummary] = useState<string>("")
+  const [isProfit, setIsProfit] = useState(true)
+  const [provider, setProvider] = useState<string>("")
+
+
 
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault()
+    console.log(name, year, profitSummary, isProfit, provider)
   }
 
 
@@ -19,18 +28,43 @@ export default function Home() {
             Business details
           </p>
           <div className="flex flex-col gap-3 mt-3">
-            <input type="text" required placeholder="Business name" className="input input-bordered w-[350px]" />
-            <input type="text" required placeholder="Year established" className="input input-bordered w-[350px]" />
-            <input type="text" required placeholder="Profit/loss summary" className="input input-bordered w-[350px]" />
+            <input
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              required
+              placeholder="Business name"
+              className="input input-bordered w-[350px]" />
+
+            <input
+              type="text"
+              onChange={(e) => setYear(e.target.value)}
+              required
+              placeholder="Year established"
+              className="input input-bordered w-[350px]" />
+
+            <input
+              type="text"
+              onChange={(e) => setProfitSummary(e.target.value)}
+              required
+              placeholder="Profit/loss summary"
+              className="input input-bordered w-[350px]" />
+
             <div className="flex flex-row items-center gap-1">
               <label htmlFor="profit-loss">Profit</label>
-              <input type="checkbox" id="profit-loss" className="checkbox" />
+              <input
+                type="checkbox"
+                checked={isProfit}
+                onChange={() => setIsProfit(!isProfit)}
+                id="profit-loss"
+                className="checkbox" />
             </div>
 
             <p className="font-lg font-bold">
               Accounting provider details
             </p>
-            <select required className="select select-bordered w-[350px]">
+            <select
+              onChange={(e) => setProvider(e.target.value)}
+              required className="select select-bordered w-[350px]">
               <option disabled selected>Choose accounting provider</option>
               {
                 accountingProviders.map((provider: string, index: number) => {
