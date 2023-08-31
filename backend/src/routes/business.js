f74e184813def1c416245849e735a493b6d0434a
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-08-31 19:46:48 +0530

    add accounting route

--- backend/src/routes/business.js
+++ backend/src/routes/business.js
@@ -3,6 +3,33 @@ const router = require("express").Router();
 const Business = require("../models/Business");
 const User = require("../models/User");
 
+const AccountingProviderData = [
+  {
+    year: 2020,
+    month: 12,
+    profitOrLoss: 250000,
+    assetsValue: 1234,
+  },
+  {
+    year: 2020,
+    month: 11,
+    profitOrLoss: 1150,
+    assetsValue: 5789,
+  },
+  {
+    year: 2020,
+    month: 10,
+    profitOrLoss: 2500,
+    assetsValue: 22345,
+  },
+  {
+    year: 2020,
+    month: 9,
+    profitOrLoss: -187000,
+    assetsValue: 223452,
+  },
+];
+
 router.post("/new", async (req, res) => {
   try {
     const { userData } = req.body;
@@ -21,6 +48,17 @@ router.post("/new", async (req, res) => {
       $push: { applicationData: response._id },
     });
     res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+router.get("/balance-sheet", async (req, res) => {
+  try {
+    const { name, ownerEmail } = req.body;
+
+    // sample balance sheet
+    res.status(200).json(AccountingProviderData);
   } catch (error) {
     res.status(500).json(error.message);
   }
